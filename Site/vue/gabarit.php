194f15b51e6ecabe9244539aba728e46a4ab111b
RedAXELight <Alexandre.baseia@cpnv.ch>
2018-06-07 09:26:19 +0200

    Bouton produits
    Ajout du bouton pour la vue des produits

--- Site/vue/gabarit.php
+++ Site/vue/gabarit.php
@@ -55,7 +55,11 @@
                     <li><a href="index.php">Accueil</a></li>
                 <?php endif; ?>
 
-                <li><a href="#about">Nos produits</a></li>
+                <?php if (@$_GET['action'] == "vue_produits") : ?>
+                    <li class="active"><a href="index.php?action=vue_produit">Nos produits</a></li>
+                <?php else : ?>
+                    <li><a href="index.php?action=vue_produits">Nos produits</a></li>
+                <?php endif; ?>
 
                 <li><a href="#service">Service</a></li>
 
